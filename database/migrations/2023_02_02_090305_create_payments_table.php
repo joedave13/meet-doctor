22a592b8999997569ddc3dd88c90483a9dcd22d5
Joshua Davian <joshuadavian@gmail.com>
2023-02-10 14:21:55 +0700

    payment model & migration

---
+++ database/migrations/2023_02_02_090305_create_payments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('appointment_id')->constrained()->cascadeOnDelete();
+            $table->integer('consultation_fee')->default(0);
+            $table->integer('doctor_fee')->default(0);
+            $table->integer('hospital_fee')->default(0);
+            $table->integer('vat')->default(0);
+            $table->integer('total')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+};
