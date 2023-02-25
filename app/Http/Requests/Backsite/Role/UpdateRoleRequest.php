11921f0a453f134f2eac091cc4687482460251c5
Joshua Davian <joshuadavian@gmail.com>
2023-02-25 09:38:58 +0700

    backsite role requests

--- app/Http/Requests/Backsite/Role/UpdateRoleRequest.php
+++ app/Http/Requests/Backsite/Role/UpdateRoleRequest.php
@@ -24,7 +24,7 @@ class UpdateRoleRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => ['required', 'string', 'max:255']
         ];
     }
 }
