ce7634a4fc692bc75a1811eb41de9502c753208b
Joshua Davian <joshuadavian@gmail.com>
2023-02-25 09:54:15 +0700

    authorize request true

--- app/Http/Requests/Backsite/Consultation/StoreConsultationRequest.php
+++ app/Http/Requests/Backsite/Consultation/StoreConsultationRequest.php
@@ -14,7 +14,7 @@ class StoreConsultationRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/Backsite/Specialist/UpdateSpecialistRequest.php
+++ app/Http/Requests/Backsite/Specialist/UpdateSpecialistRequest.php
@@ -13,7 +13,7 @@ class UpdateSpecialistRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
