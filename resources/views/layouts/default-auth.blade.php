955e0112f2d1ffb0826734e41e5ada7ec17906b5
Joshua Davian <joshuadavian@gmail.com>
2023-02-25 18:35:51 +0700

    custom login & register page

---
+++ resources/views/layouts/default-auth.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>{{ $title ?? '' }} | Meet Doctor</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet" />
+
+    <link rel="stylesheet" href="{{ asset('assets/frontsite/main.css') }}" />
+
+    @stack('styles')
+</head>
+
+<body>
+    @yield('content')
+
+    @stack('scripts')
+</body>
+
+</html>
