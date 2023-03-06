c5d6521fe71cfc7fdfbadbce1e8e8a1b176e96fd
Joshua Davian <joshuadavian@gmail.com>
2023-03-06 15:27:53 +0700

    backsite delete user

--- resources/views/pages/backsite/user/table-action.blade.php
+++ resources/views/pages/backsite/user/table-action.blade.php
@@ -6,7 +6,8 @@
     class="inline-block border border-indigo-500 bg-indigo-500 text-white rounded-md px-2 py-1 m-1 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline">
     <i class="fas fa-fw fa-edit"></i>
 </a>
-<form class="inline-block" action="{{ route('backsite.user.destroy', $id) }}" method="POST">
+<form class="inline-block" action="{{ route('backsite.user.destroy', $id) }}" method="POST"
+    onsubmit="return confirm('Are you sure to delete this user?');">
     @csrf
     @method('DELETE')
     <button type="submit"
