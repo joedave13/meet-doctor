f199bb34784da834e1ca56d18bceda6666fd84dc
Joshua Davian <joshuadavian@gmail.com>
2023-03-08 12:55:39 +0700

    backsite payment permission

--- app/Http/Controllers/Backsite/PaymentController.php
+++ app/Http/Controllers/Backsite/PaymentController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Models\Payment;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use Symfony\Component\HttpFoundation\Response;
 
 class PaymentController extends Controller
 {
@@ -16,6 +18,8 @@ class PaymentController extends Controller
      */
     public function index()
     {
+        abort_if(Gate::denies('payment_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
         if (request()->ajax()) {
             $payments = Payment::query()->latest();
 
@@ -60,6 +64,8 @@ class PaymentController extends Controller
      */
     public function show(Payment $payment)
     {
+        abort_if(Gate::denies('payment_show'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
         return view('pages.backsite.payment.show', compact('payment'));
     }
 
